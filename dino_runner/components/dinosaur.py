eee762fbf95b6e262f55743c5aef52391505c863
FerPinto1406 <Fernando.Pinto@fundacion-jala.org>
2022-08-08 21:35:20 -0400

    TAREA 1: Ducking
    Creación del método Duck.

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,11 +1,13 @@
 import pygame
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 from pygame.sprite import Sprite
 
 class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
+    Y_POS_DUCK = 340 
     JUMP_VEL = 8.5
+    
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -14,6 +16,7 @@ class Dinosaur(Sprite):
         self.step_index = 0
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_vel = self.JUMP_VEL
 
     def update(self, user_input):
@@ -21,12 +24,21 @@ class Dinosaur(Sprite):
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
         elif not self.dino_jump:
             self.dino_jump = False
+            self.dino_run = True
+
+        if user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+        elif not self.dino_duck:
+            self.dino_duck = False
             self.dino_run = True
 
         if self.step_index >= 10:
@@ -51,7 +63,11 @@ class Dinosaur(Sprite):
             self.jump_vel = self.JUMP_VEL
 
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index +=1
 
     def draw(self, screen: pygame.Surface):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
